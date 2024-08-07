7b3929fbad10f859eda01ac24636f89daeee3dd2
Abhinav Shakya <136550261+abhinav-123457@users.noreply.github.com>
2024-08-07 10:20:54 +0530

    Delete FirebaseConfig.js

--- FirebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCr3mxKjPSXV710z_V0uVWerU7oiH4R0O8",
-    authDomain: "dtc-2f240.firebaseapp.com",
-    projectId: "dtc-2f240",
-    storageBucket: "dtc-2f240.appspot.com",
-    messagingSenderId: "978063616113",
-    appId: "1:978063616113:web:a306ab5e6f5338780213d0",
-    measurementId: "G-RWH00FR586"
-};
